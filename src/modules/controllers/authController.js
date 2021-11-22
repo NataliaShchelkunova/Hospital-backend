4fa94346e83a2b60f94871da15b9bde9a44e4e59
NataliaShchelkunova <>
2021-11-22 14:25:17 +0300

    delete login router

--- src/modules/controllers/authController.js
+++ src/modules/controllers/authController.js
@@ -1,16 +1,6 @@
 const User = require("../../models/user/User");
 const bcrypt = require("bcryptjs");
-const jwt = require("jsonwebtoken");
-const { secret } = require("./config");
 const { validationResult } = require("express-validator");
-
-const generateAccecToken = (id, username) => {
-  const payload = {
-    id,
-    username,
-  };
-  return jwt.sign(payload, secret, { expiresIn: "24h" });
-};
 
 module.exports.registration = async (req, res) => {
   const errors = validationResult(req);
@@ -26,36 +16,19 @@ module.exports.registration = async (req, res) => {
       const hashPassword = bcrypt.hashSync(password, 7);
       const user = new User({ username, password: hashPassword });
       await user.save();
-      const token = generateAccecToken(user._id, username);
-      return res.json({ message: "User registered successfully", token });
+      return res.json({ message: "User registered successfully" });
     } catch (error) {
+      console.log(error);
       res.status(400).json({ message: "Registration error" });
     }
   } else {
-    res.status(400).json({ message: "Check all field ", errors });
+    res.status(400).json({ message: "Check all field " });
   }
 };
 
 module.exports.login = async (req, res) => {
-  const errors = validationResult(req);
   try {
-    const { username, password } = req.body;
-    const user = await User.findOne({ username });
-
-    if (user) {
-      const validPassword = bcrypt.compareSync(password, user.password);
-      if (validPassword) {
-        const token = generateAccecToken(user._id, username);
-        return res.json({ token, username });
-      } else {
-        return res.status(400).json({ message: `Password is uncorrect` });
-      }
-    } else {
-      return res.status(400).json({ message: `user ${username} is defined` });
-    }
-  } catch (errors) {
-    res.status(400).json({ message: "Autorisation error", errors: errors });
-  }
+  } catch (error) {}
 };
 
 module.exports.getUsers = async (req, res) => {
